17ffa1fa65c681c2d61cf71fca398adc6d576a3a
jeongwusi <jeongwusi@gmail.com>
2024-05-08 16:13:20 +0900

    ✨ feat: 유저 이름에 유니크한 값 주기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -15,7 +19,13 @@ export class AuthService {
     const { username, password } = authCredentialsDto;
     const user = this.userRepository.create({ username, password });
 
-    await this.userRepository.save(user);
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505')
+        throw new ConflictException('이미 존재하는 닉네임입니다.');
+      else throw new InternalServerErrorException();
+    }
     return user;
   }
 }
